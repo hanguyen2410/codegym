48042cc5b8f5a3acb4faba631c9b0f85baca6866
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-07-17 13:11:23 +0700

    bai9

---
+++ bai9/kiemtranamnhuan.js
@@ -0,0 +1,30 @@
+let year = parseInt(prompt("Enter a year"));
+let isLeapYear = false;
+
+//if (year % 4 == 0) {
+  //  if (year % 100 == 0) {
+  //      if (year % 400 ==0) {
+   //         alert(year + " is a leap year");
+  //      } else {
+  //          alert(year + " is NOT a leap year")
+  //      }
+  //  } else {
+  //      alert(year + " is a leap year");
+  //  }
+//} else {
+    //alert(year + " is NOT a leap year");
+//}
+if (year % 4 == 0) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            isLeapYear = true;
+        }
+    } else {
+        isLeapYear = true;
+    }
+}
+if (isLeapYear) {
+    alert (year + " is Leap Year");
+} else {
+    alert (year + " is NOT Leap Year");
+}
