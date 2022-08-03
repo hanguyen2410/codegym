9bfeae1b1a833aaac8bb8c21d1d4641bbdddf4e0
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-08-03 23:56:06 +0700

    case study

--- case study/casestudy1.js
+++ case study/casestudy1.js
@@ -1,3 +1,7 @@
+let loged = false;
+const enterKey = 13;
+var orderList = [];
+
 //top
 let menuBar = document.querySelector(".menu-bar")
 menuBar.addEventListener("click", function () {
@@ -56,6 +60,7 @@ function renderMorningFoods() {
     let htmls = "";
     for (let i = 0; i < foodsMorning.length; i++) {
         htmls += `
+            <div id="div-${[i]}" class="div-item">
                 <div class="food-items">
                         <div class="food-item">
                         <img src="${imagesMorning[i]}">
@@ -65,11 +70,12 @@ function renderMorningFoods() {
                             <p>${pricesMorning[i]}</p>
                         </div>
                     </div>
-                    <div class="button-edit">
-                    <button class="btn-menu" id="removeMenuMorning" onclick="removeMorning(${i})">
+                    <div class="button-remove">
+                    <button class="btn-menu removeMenuMorning ${loged == true ? '' : 'd-none'}" onclick="removeMorning(${i})">
                         Remove
                     </button>
                 </div>
+            </div>
         `;
     }
     document.querySelector('#morning').innerHTML = htmls;
@@ -93,7 +99,7 @@ function renderLunchFoods() {
                         </div>
                     </div>
                     <div class="button-edit">
-                    <button class="btn-menu" id="removeMenuLunch" onclick="removeLunch(${i})">
+                    <button class="btn-menu removeMenuLunch ${loged == true ? '' : 'd-none'}" onclick="removeLunch(${i})">
                         Remove
                     </button>
                 </div>
@@ -118,7 +124,7 @@ function renderNightFoods() {
                         </div>
                     </div>
                     <div class="button-edit">
-                    <button class="btn-menu" id="removeMenuNight" onclick="removeNight(${i})">
+                    <button class="btn-menu removeMenuNight ${loged == true ? '' : 'd-none'}"  onclick="removeNight(${i})">
                         Remove
                     </button>
                 </div>
@@ -143,7 +149,7 @@ function renderDrink() {
                         </div>
                     </div>
                     <div class="button-edit">
-                    <button class="btn-menu" id="removeMenuDrink" onclick="removeDrink(${i})">
+                    <button class="btn-menu removeMenuDrink ${loged == true ? '' : 'd-none'}"  onclick="removeDrink(${i})">
                         Remove
                     </button>
                 </div>
@@ -276,7 +282,7 @@ function orderName(day, name, time, phone, person, email) {
     this.person = person;
     this.email = email;
 }
-var orderList = [];
+
 function orderButton() {
 
     let day = document.querySelector(`#dateOrder`).value;
@@ -285,13 +291,13 @@ function orderButton() {
     let phone = document.querySelector(`#phoneOrder`).value;
     let person = document.querySelector(`#personOrder`).value;
     let email = document.querySelector(`#emailOrder`).value;
-    if (day == '' ||name == '' ||time == '' ||phone == '' ||person == '' ||email == '') {
+    if (day == '' || name == '' || time == '' || phone == '' || person == '' || email == '') {
         alert('Input is required?')
         return
     }
-    let newBooking = new orderName(day, name, time, phone, person, email)
+    let newBooking = new orderName(day, name, time, phone, person, email);
     orderList.push(newBooking);
-    
+    setData(key_order, orderList);
     document.querySelector(`#dateOrder`).value = null;
     document.querySelector(`#nameOrder`).value = null;
     document.querySelector(`#timeOrder`).value = null;
@@ -306,7 +312,7 @@ function renderOrderList() {
     let htmls = "";
     for (let [index, booking] of orderList.entries()) {
 
-        htmls += `<tr>
+        htmls += `<tr class="tr_${index}">
             <td>${index + 1}</td>
             <td>${booking.day}</td>
             <td>${booking.name}</td>
@@ -314,9 +320,202 @@ function renderOrderList() {
             <td>${booking.phone}</td>
             <td>${booking.person}</td>
             <td>${booking.email}</td>
+            <td>
+            <button class="btn-booking edit-booking${index} " onclick="editBooking(${index})">Edit</button>
+            <button class="btn-booking save-booking${index} d-none" onclick="saveBooking(${index})">Save</button>
+            <button class="btn-booking cancel-booking${index} d-none" onclick="cancelBooking(${index})">Cancel</button>
+            <button class="btn-booking remove-booking${index} " onclick="removeBooking(${index})">Remove</button>
+            </td>
         </tr>
             `
     }
     document.querySelector('.tboy-order').innerHTML = htmls;
 }
+function removeBooking(index) {
+    let confirmed = window.confirm('Are you sure to remove this?');
+    if(confirmed) {
+        orderList.splice(index,1);
+        setData(key_order, orderList);
+        renderOrderList();
+    }
+}
+function editBooking(index){
+    let tr = document.querySelector(`.tr_${index}`);
+    let oldDate = tr.children[1].innerHTML;
+    let oldName = tr.children[2].innerHTML;
+    let oldTime = tr.children[3].innerHTML;
+    let oldPhone = tr.children[4].innerHTML;
+    let oldPerson = tr.children[5].innerHTML;
+    let oldEmail = tr.children[6].innerHTML;
+    tr.children[1].innerHTML=`<input type="text" id="old-Date${index}" value="${oldDate}">`;
+    tr.children[2].innerHTML=`<input type="text" id="old-Name${index}" value="${oldName}">`;
+    tr.children[3].innerHTML=`<input type="text" id="old-Time${index}" value="${oldTime}">`;
+    tr.children[4].innerHTML=`<input type="text" id="old-Phone${index}" value="${oldPhone}">`;
+    tr.children[5].innerHTML=`<input type="text" id="old-Person${index}" value="${oldPerson}">`;
+    tr.children[6].innerHTML=`<input type="text" id="old-Email${index}" value="${oldEmail}">`;
+    setData(key_order, orderList);
+    document.querySelector(`.save-booking${index}`).classList.remove("d-none");
+    document.querySelector(`.cancel-booking${index}`).classList.remove("d-none");
+    document.querySelector(`.edit-booking${index}`).classList.add("d-none");
+}
+function saveBooking(index){
+    
+    let tr = document.querySelector(`.tr_${index}`);
+    let oldDate = document.querySelector(`#old-Date${index}`).value;
+    tr.children[1].innerHTML = oldDate;
+    let oldName = document.querySelector(`#old-Name${index}`).value;
+    tr.children[2].innerHTML = oldName;
+    let oldTime = document.querySelector(`#old-Time${index}`).value;
+    tr.children[3].innerHTML = oldTime;
+    let oldPhone = document.querySelector(`#old-Phone${index}`).value;
+    tr.children[4].innerHTML = oldPhone;
+    let oldPerson = document.querySelector(`#old-Person${index}`).value;
+    tr.children[5].innerHTML = oldPerson;
+    let oldEmail = document.querySelector(`#old-Email${index}`).value;
+    tr.children[6].innerHTML = oldEmail;
+    setData(key_order, orderList);
+    document.querySelector(`.save-booking${index}`).classList.add("d-none");
+    document.querySelector(`.cancel-booking${index}`).classList.add("d-none");
+    document.querySelector(`.edit-booking${index}`).classList.remove("d-none");
+}
+function cancelBooking(index){
+    let tr = document.querySelector(`.tr_${index}`);
+    let oldDate = document.querySelector(`#old-Date${index}`).value;
+    tr.children[1].innerHTML = oldDate;
+    let oldName = document.querySelector(`#old-Name${index}`).value;
+    tr.children[2].innerHTML = oldName;
+    let oldTime = document.querySelector(`#old-Time${index}`).value;
+    tr.children[3].innerHTML = oldTime;
+    let oldPhone = document.querySelector(`#old-Phone${index}`).value;
+    tr.children[4].innerHTML = oldPhone;
+    let oldPerson = document.querySelector(`#old-Person${index}`).value;
+    tr.children[5].innerHTML = oldPerson;
+    let oldEmail = document.querySelector(`#old-Email${index}`).value;
+    tr.children[6].innerHTML = oldEmail;
+    document.querySelector(`.save-booking${index}`).classList.add("d-none");
+    document.querySelector(`.cancel-booking${index}`).classList.add("d-none");
+    document.querySelector(`.edit-booking${index}`).classList.remove("d-none");
+}
 
+// passWord-------------------------------------------
+function Login(username, password) {
+    this.username = username;
+    this.password = password;
+}
+var loginList = [new Login('admin', 'admin'), new Login('123123', '123123')];
+
+// let removeMenuMorning = [];
+
+
+function checkLogin() {
+    let username = document.querySelector(`#userName_2`).value;
+    let password = document.querySelector(`#passWord_2`).value;
+    let newLogin = new Login(username, password);
+    // for (let i = 0; i <= loginList.length; i++) {
+    //     for (let j = 0; j < loginList[i].length; j++) {
+    //         if (username == loginList[i][j] && password == loginList[i][j + 1]) {
+    //             alert('Đăng Nhập Thành Công!')
+    //             break;
+    //         } else {
+    //             alert('Đăng Nhập Thất Bại Vui Lòng Thử Lại');
+    //             continue;
+
+    //         }
+    //     }
+
+    // }
+    
+    for (let login of loginList) {
+        if (username == login.username && password == login.password) {
+            loged = true;
+            break;
+        }
+    }
+    if (loged) {
+        alert('Đăng Nhập Thành Công!')
+        document.querySelector("#foodName").classList.remove('d-none');
+        document.querySelector("#addNameMenu").classList.remove('d-none');
+        document.querySelector("#priceName").classList.remove('d-none');
+        document.querySelector("#addPricesMenu").classList.remove('d-none');
+        document.querySelector("#imagesName").classList.remove('d-none');
+        document.querySelector("#addImgMenu").classList.remove('d-none');
+        document.querySelector("#addMorning").classList.remove('d-none');
+        document.querySelector("#addLunch").classList.remove('d-none');
+        document.querySelector("#addNight").classList.remove('d-none');
+        document.querySelector("#addDrink").classList.remove('d-none');
+
+        let removeMenuMorning = document.querySelectorAll(".removeMenuMorning");
+
+        removeMenuMorning.forEach((item, index) => {
+            item.classList.remove("d-none");
+        });
+        let removeMenuLunch = document.querySelectorAll(".removeMenuLunch");
+
+        removeMenuLunch.forEach((item, index) => {
+            item.classList.remove("d-none");
+        });
+        let removeMenuNight = document.querySelectorAll(".removeMenuNight");
+
+        removeMenuNight.forEach((item, index) => {
+            item.classList.remove("d-none");
+        });
+        let removeMenuDrink = document.querySelectorAll(".removeMenuDrink");
+
+        removeMenuDrink.forEach((item, index) => {
+            item.classList.remove("d-none");
+        });
+
+        // document.querySelector(".checkOrder").classList.remove('d-none');
+
+        document.querySelector(`#userName_2`).value = null;
+        document.querySelector(`#passWord_2`).value = null;
+        document.querySelector('#btn-checklogin').classList.add('d-none');
+        document.querySelector('#userName_1').classList.add('d-none');
+        document.querySelector('#userName_2').classList.add('d-none');
+        document.querySelector('#passWord_1').classList.add('d-none');
+        document.querySelector('#passWord_2').classList.add('d-none');
+
+        // document.querySelector("#removeMenuLunch").classList.remove('d-none');
+        // document.querySelector("#removeMenuNight").classList.remove('d-none');
+        // document.querySelector("#removeMenuDrink").classList.remove('d-none');
+    }
+    else {
+        alert('Đăng Nhập Thất Bại Vui Lòng Thử Lại');
+    }
+}
+
+function openLogin() {
+    document.querySelector('#btn-checklogin').classList.remove('d-none');
+    document.querySelector('#userName_1').classList.remove('d-none');
+    document.querySelector('#userName_2').classList.remove('d-none');
+    document.querySelector('#passWord_1').classList.remove('d-none');
+    document.querySelector('#passWord_2').classList.remove('d-none');
+}
+// Nut Enter
+function pressEnter(i) {
+    if (i.keyCode == enterKey) {
+        orderButton();
+
+    }
+}
+// localStorage
+let key_order = "order_data";
+function init(){
+    if(getData(key_order) == null){
+        orderList = []
+        setData(key_order, orderList);
+    } else {
+        orderList = getData(key_order);
+    }
+}
+function getData(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+function setData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+function mainBooking(){
+    init();
+    renderOrderList();
+}
+mainBooking();
