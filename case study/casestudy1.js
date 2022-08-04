c77f764a5091bfdbc12b061728126fd48a40f65d
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-08-04 11:11:17 +0700

    case study

--- case study/casestudy1.js
+++ case study/casestudy1.js
@@ -53,21 +53,26 @@ function drink() {
     document.querySelector(`#night`).classList.add('d-none');
     document.querySelector(`#drink`).classList.remove('d-none');
 }
-const foodsMorning = ['Bánh Mì Xíu Mại', 'Bún Bò Huế', 'Phở Bò', 'Xôi Gà', 'Bánh Cuốn Hà Nội', 'Bún Hến', 'Mì Tôm Hến', 'Cháo Gà']
-const imagesMorning = ['ansang/ansang1.jpg', 'ansang/ansang2.jpg', 'ansang/ansang3.jpg', 'ansang/ansang4.jpg', 'ansang/ansang5.jpg', 'ansang/ansang6.jpg', 'ansang/ansang7.jpg', 'ansang/ansang8.jpg']
-const pricesMorning = ['15.000 VNĐ', '25.000 VNĐ', '25.000 VNĐ', '25.000 VNĐ', '20.000 VNĐ', '10.000 VNĐ', '10.000 VNĐ', '20.000 VNĐ']
+let foodsMorning = ['Bánh Mì Xíu Mại', 'Bún Bò Huế', 'Phở Bò', 'Xôi Gà', 'Bánh Cuốn Hà Nội', 'Bún Hến', 'Mì Tôm Hến', 'Cháo Gà']
+let imagesMorning = ['ansang/ansang1.jpg', 'ansang/ansang2.jpg', 'ansang/ansang3.jpg', 'ansang/ansang4.jpg', 'ansang/ansang5.jpg', 'ansang/ansang6.jpg', 'ansang/ansang7.jpg', 'ansang/ansang8.jpg']
+let pricesMorning = ['15.000 VNĐ', '25.000 VNĐ', '25.000 VNĐ', '25.000 VNĐ', '20.000 VNĐ', '10.000 VNĐ', '10.000 VNĐ', '20.000 VNĐ']
+var menu_Morning = {
+    foodsMorning: foodsMorning,
+    imagesMorning: imagesMorning,
+    pricesMorning: pricesMorning
+}
 function renderMorningFoods() {
     let htmls = "";
-    for (let i = 0; i < foodsMorning.length; i++) {
+    for (let i = 0; i < menu_Morning.foodsMorning.length; i++) {
         htmls += `
             <div id="div-${[i]}" class="div-item">
                 <div class="food-items">
                         <div class="food-item">
-                        <img src="${imagesMorning[i]}">
-                        <p>${foodsMorning[i]}</p>
+                        <img src="${menu_Morning.imagesMorning[i]}">
+                        <p>${menu_Morning.foodsMorning[i]}</p>
                         </div>
                         <div class="food-price">
-                            <p>${pricesMorning[i]}</p>
+                            <p>${menu_Morning.pricesMorning[i]}</p>
                         </div>
                     </div>
                     <div class="button-remove">
@@ -84,18 +89,22 @@ function renderMorningFoods() {
 const foodsLunch = ['Tôm Càng Xanh', 'Gà Đồi Hấp Hành', 'Cá Vược Hấp Cà', 'Sườn Chua Ngọt', 'Rau Muống Xào Tỏi', 'Canh Chua Cá', 'Canh Rau', 'Bò Xào Hành Tây']
 const imagesLunch = ['anhmenu/anhmenu1.jpg', 'anhmenu/anhmenu2.jpg', 'anhmenu/anhmenu3.jpg', 'anhmenu/anhmenu4.jpg', 'anhmenu/anhmenu5.jpg', 'anhmenu/anhmenu6.jpg', 'anhmenu/anhmenu7.jpg', 'anhmenu/anhmenu8.jpg']
 const pricesLunch = ['300.000 VNĐ', '350.000 VNĐ', '150.000 VNĐ', '150.000 VNĐ', '30.000 VNĐ', '150.000 VNĐ', '40.000 VNĐ', '200.000 VNĐ']
-
+var menu_Lunch = {
+    foodsLunch: foodsLunch,
+    imagesLunch: imagesLunch,
+    pricesLunch: pricesLunch
+}
 function renderLunchFoods() {
     let htmls = "";
-    for (let i = 0; i < foodsLunch.length; i++) {
+    for (let i = 0; i < menu_Lunch.foodsLunch.length; i++) {
         htmls += `
                 <div class="food-items">
                         <div class="food-item">
-                        <img src="${imagesLunch[i]}">
-                        <p>${foodsLunch[i]}</p>
+                        <img src="${menu_Lunch.imagesLunch[i]}">
+                        <p>${menu_Lunch.foodsLunch[i]}</p>
                         </div>
                         <div class="food-price">
-                            <p>${pricesLunch[i]}</p>
+                            <p>${menu_Lunch.pricesLunch[i]}</p>
                         </div>
                     </div>
                     <div class="button-edit">
@@ -110,17 +119,22 @@ function renderLunchFoods() {
 const foodsNight = ['Sườn Chua Ngọt', 'Bò Xào Hành Tây', 'Cá Vược Hấp Cà', 'Tôm Càng Xanh', 'Canh Rau', 'Canh Chua Cá', 'Rau Muống Xào Tỏi', 'Gà Đồi Hấp Hành']
 const imagesNight = ['anhmenu/anhmenu4.jpg', 'anhmenu/anhmenu8.jpg', 'anhmenu/anhmenu3.jpg', 'anhmenu/anhmenu1.jpg', 'anhmenu/anhmenu7.jpg', 'anhmenu/anhmenu6.jpg', 'anhmenu/anhmenu5.jpg', 'anhmenu/anhmenu2.jpg']
 const pricesNight = ['150.000 VNĐ', '200.000 VNĐ', '150.000 VNĐ', '300.000 VNĐ', '40.000 VNĐ', '150.000 VNĐ', '30.000 VNĐ', '350.000 VNĐ']
+var menu_Night = {
+    foodsNight: foodsNight,
+    imagesNight: imagesNight,
+    pricesNight: pricesNight
+}
 function renderNightFoods() {
     let htmls = "";
-    for (let i = 0; i < foodsNight.length; i++) {
+    for (let i = 0; i < menu_Night.foodsNight.length; i++) {
         htmls += `
                 <div class="food-items">
                         <div class="food-item">
-                        <img src="${imagesNight[i]}">
-                        <p>${foodsNight[i]}</p>
+                        <img src="${menu_Night.imagesNight[i]}">
+                        <p>${menu_Night.foodsNight[i]}</p>
                         </div>
                         <div class="food-price">
-                            <p>${pricesNight[i]}</p>
+                            <p>${menu_Night.pricesNight[i]}</p>
                         </div>
                     </div>
                     <div class="button-edit">
@@ -135,17 +149,22 @@ function renderNightFoods() {
 const drinks = ['Coca-cola', 'Pepsi', 'Mirinda', 'Redbull', 'Bia Huda', 'Bia Sài Gòn', 'Bia Heineken', 'Bia Tiger']
 const imagesDrink = ['douong/douong1.jpg', 'douong/douong2.jpg', 'douong/douong3.jpg', 'douong/douong4.jpg', 'douong/douong5.jpg', 'douong/douong6.jpg', 'douong/douong7.jpg', 'douong/douong8.jpg']
 const pricesDrink = ['15.000 VNĐ', '15.000 VNĐ', '15.000 VNĐ', '15.000 VNĐ', '15.000 VNĐ', '15.000 VNĐ', '25.000 VNĐ', '25.000 VNĐ']
+var menu_Drink = {
+    drinks: drinks,
+    imagesDrink: imagesDrink,
+    pricesDrink: pricesDrink
+}
 function renderDrink() {
     let htmls = "";
-    for (let i = 0; i < drinks.length; i++) {
+    for (let i = 0; i < menu_Drink.drinks.length; i++) {
         htmls += `
                 <div class="food-items">
                         <div class="food-item">
-                        <img src="${imagesDrink[i]}">
-                        <p>${drinks[i]}</p>
+                        <img src="${menu_Drink.imagesDrink[i]}">
+                        <p>${menu_Drink.drinks[i]}</p>
                         </div>
                         <div class="food-price">
-                            <p>${pricesDrink[i]}</p>
+                            <p>${menu_Drink.pricesDrink[i]}</p>
                         </div>
                     </div>
                     <div class="button-edit">
@@ -157,9 +176,52 @@ function renderDrink() {
     }
     document.querySelector('#drink').innerHTML = htmls;
 }
+// localStorage
+const key_MenuMorning = "key_menuMoring";
+const key_MenuLunch = "key_menLunch";
+const key_MenuNight = "key_menuNight";
+const key_MenuDrink = "key_menuDrink";
+function getData(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+function setData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+function initMenuMorning() {
+    if (getData(key_MenuMorning) == null) {
+        setData(key_MenuMorning, menu_Morning);
+    } else {
+        menu_Morning = getData(key_MenuMorning);
+    }
+}
+function initMenuLunch() {
+    if (getData(key_MenuLunch) == null) {
+        setData(key_MenuLunch, menu_Lunch);
+    } else {
+        menu_Lunch = getData(key_MenuLunch);
+    }
+}
+function initMenuNight() {
+    if (getData(key_MenuNight) == null) {
+        setData(key_MenuNight, menu_Night);
+    } else {
+        menu_Night = getData(key_MenuNight);
+    }
+}
+function initMenuDrink() {
+    if (getData(key_MenuDrink) == null) {
+        setData(key_MenuDrink, menu_Drink);
+    } else {
+        menu_Drink = getData(key_MenuDrink);
+    }
+}
 
 
 function mainMenu() {
+    initMenuDrink();
+    initMenuNight();
+    initMenuLunch();
+    initMenuMorning();
     renderMorningFoods();
     renderLunchFoods();
     renderNightFoods();
@@ -169,42 +231,43 @@ mainMenu();
 
 
 
-function addMorning() {
-
-}
 function removeMorning(index) {
     let confirmed = window.confirm("Are you sure to remove this?");
     if (confirmed) {
-        foodsMorning.splice(index, 1);
-        imagesMorning.splice(index, 1);
-        pricesMorning.splice(index, 1);
+        menu_Morning.foodsMorning.splice(index, 1);
+        menu_Morning.imagesMorning.splice(index, 1);
+        menu_Morning.pricesMorning.splice(index, 1);
+        setData(key_MenuMorning, menu_Morning);
         renderMorningFoods();
     }
 }
 function removeLunch(index) {
     let confirmed = window.confirm("Are you sure to remove this?");
     if (confirmed) {
-        foodsLunch.splice(index, 1);
-        imagesLunch.splice(index, 1);
-        pricesLunch.splice(index, 1);
+        menu_Lunch.foodsLunch.splice(index, 1);
+        menu_Lunch.imagesLunch.splice(index, 1);
+        menu_Lunch.pricesLunch.splice(index, 1);
+        setData(key_MenuLunch, menu_Lunch);
         renderLunchFoods();
     }
 }
 function removeNight(index) {
     let confirmed = window.confirm("Are you sure to remove this?");
     if (confirmed) {
-        foodsNight.splice(index, 1);
-        imagesNight.splice(index, 1);
-        pricesNight.splice(index, 1);
+        menu_Night.foodsNight.splice(index, 1);
+        menu_Night.imagesNight.splice(index, 1);
+        menu_Night.pricesNight.splice(index, 1);
+        setData(key_MenuNight, menu_Night);
         renderNightFoods();
     }
 }
 function removeDrink(index) {
     let confirmed = window.confirm("Are you sure to remove this?");
     if (confirmed) {
-        drinks.splice(index, 1);
-        imagesDrink.splice(index, 1);
-        pricesDrink.splice(index, 1);
+        menu_Drink.drinks.splice(index, 1);
+        menu_Drink.imagesDrink.splice(index, 1);
+        menu_Drink.pricesDrink.splice(index, 1);
+        setData(key_MenuDrink, menu_Drink);
         renderDrink();
     }
 }
@@ -216,9 +279,10 @@ function addMorning(index) {
         alert('Input is required?')
         return
     }
-    foodsMorning.push(foodAddName);
-    imagesMorning.push(priceAddName);
-    pricesMorning.push(imageAddName);
+    menu_Morning.foodsMorning.push(foodAddName);
+    menu_Morning.imagesMorning.push(priceAddName);
+    menu_Morning.pricesMorning.push(imageAddName);
+    setData(key_MenuMorning, menu_Morning);
     renderMorningFoods();
     document.querySelector(`#addNameMenu`).value = null;
     document.querySelector(`#addPricesMenu`).value = null;
@@ -232,9 +296,10 @@ function addLunch(index) {
         alert('Input is required?')
         return
     }
-    foodsLunch.push(foodAddName);
-    imagesLunch.push(priceAddName);
-    pricesLunch.push(imageAddName);
+    menu_Lunch.foodsLunch.push(foodAddName);
+    menu_Lunch.imagesLunch.push(priceAddName);
+    menu_Lunch.pricesLunch.push(imageAddName);
+    setData(key_MenuLunch, menu_Lunch);
     renderLunchFoods();
     document.querySelector(`#addNameMenu`).value = null;
     document.querySelector(`#addPricesMenu`).value = null;
@@ -248,9 +313,10 @@ function addNight(index) {
         alert('Input is required?')
         return
     }
-    foodsNight.push(foodAddName);
-    imagesNight.push(priceAddName);
-    pricesNight.push(imageAddName);
+    menu_Night.foodsNight.push(foodAddName);
+    menu_Night.imagesNight.push(priceAddName);
+    menu_Night.pricesNight.push(imageAddName);
+    setData(key_MenuNight, menu_Night);
     renderNightFoods();
     document.querySelector(`#addNameMenu`).value = null;
     document.querySelector(`#addPricesMenu`).value = null;
@@ -265,9 +331,10 @@ function addDrink(index) {
         return
     }
 
-    drinks.push(foodAddName);
-    imagesDrink.push(priceAddName);
-    pricesDrink.push(imageAddName);
+    menu_Drink.drinks.push(foodAddName);
+    menu_Drink.imagesDrink.push(priceAddName);
+    menu_Drink.pricesDrink.push(imageAddName);
+    setData(key_MenuDrink, menu_Drink);
     renderDrink();
     document.querySelector(`#addNameMenu`).value = null;
     document.querySelector(`#addPricesMenu`).value = null;
@@ -333,13 +400,13 @@ function renderOrderList() {
 }
 function removeBooking(index) {
     let confirmed = window.confirm('Are you sure to remove this?');
-    if(confirmed) {
-        orderList.splice(index,1);
+    if (confirmed) {
+        orderList.splice(index, 1);
         setData(key_order, orderList);
         renderOrderList();
     }
 }
-function editBooking(index){
+function editBooking(index) {
     let tr = document.querySelector(`.tr_${index}`);
     let oldDate = tr.children[1].innerHTML;
     let oldName = tr.children[2].innerHTML;
@@ -347,51 +414,53 @@ function editBooking(index){
     let oldPhone = tr.children[4].innerHTML;
     let oldPerson = tr.children[5].innerHTML;
     let oldEmail = tr.children[6].innerHTML;
-    tr.children[1].innerHTML=`<input type="text" id="old-Date${index}" value="${oldDate}">`;
-    tr.children[2].innerHTML=`<input type="text" id="old-Name${index}" value="${oldName}">`;
-    tr.children[3].innerHTML=`<input type="text" id="old-Time${index}" value="${oldTime}">`;
-    tr.children[4].innerHTML=`<input type="text" id="old-Phone${index}" value="${oldPhone}">`;
-    tr.children[5].innerHTML=`<input type="text" id="old-Person${index}" value="${oldPerson}">`;
-    tr.children[6].innerHTML=`<input type="text" id="old-Email${index}" value="${oldEmail}">`;
+    tr.children[1].innerHTML = `<input type="text" id="old-Date${index}" value="${oldDate}">`;
+    tr.children[2].innerHTML = `<input type="text" id="old-Name${index}" value="${oldName}">`;
+    tr.children[3].innerHTML = `<input type="text" id="old-Time${index}" value="${oldTime}">`;
+    tr.children[4].innerHTML = `<input type="text" id="old-Phone${index}" value="${oldPhone}">`;
+    tr.children[5].innerHTML = `<input type="text" id="old-Person${index}" value="${oldPerson}">`;
+    tr.children[6].innerHTML = `<input type="text" id="old-Email${index}" value="${oldEmail}">`;
     setData(key_order, orderList);
     document.querySelector(`.save-booking${index}`).classList.remove("d-none");
     document.querySelector(`.cancel-booking${index}`).classList.remove("d-none");
     document.querySelector(`.edit-booking${index}`).classList.add("d-none");
 }
-function saveBooking(index){
-    
+function saveBooking(index) {
+
     let tr = document.querySelector(`.tr_${index}`);
-    let oldDate = document.querySelector(`#old-Date${index}`).value;
-    tr.children[1].innerHTML = oldDate;
-    let oldName = document.querySelector(`#old-Name${index}`).value;
-    tr.children[2].innerHTML = oldName;
-    let oldTime = document.querySelector(`#old-Time${index}`).value;
-    tr.children[3].innerHTML = oldTime;
-    let oldPhone = document.querySelector(`#old-Phone${index}`).value;
-    tr.children[4].innerHTML = oldPhone;
-    let oldPerson = document.querySelector(`#old-Person${index}`).value;
-    tr.children[5].innerHTML = oldPerson;
-    let oldEmail = document.querySelector(`#old-Email${index}`).value;
-    tr.children[6].innerHTML = oldEmail;
+    let newDate = document.querySelector(`#old-Date${index}`).value;
+    tr.children[1].innerHTML = newDate;
+    let newName = document.querySelector(`#old-Name${index}`).value;
+    tr.children[2].innerHTML = newName;
+    let newTime = document.querySelector(`#old-Time${index}`).value;
+    tr.children[3].innerHTML = newTime;
+    let newPhone = document.querySelector(`#old-Phone${index}`).value;
+    tr.children[4].innerHTML = newPhone;
+    let newPerson = document.querySelector(`#old-Person${index}`).value;
+    tr.children[5].innerHTML = newPerson;
+    let newEmail = document.querySelector(`#old-Email${index}`).value;
+    tr.children[6].innerHTML = newEmail;
+    orderList[index] = new orderName(newDate, newName, newTime, newPhone, newPerson, newEmail)
     setData(key_order, orderList);
     document.querySelector(`.save-booking${index}`).classList.add("d-none");
     document.querySelector(`.cancel-booking${index}`).classList.add("d-none");
     document.querySelector(`.edit-booking${index}`).classList.remove("d-none");
 }
-function cancelBooking(index){
-    let tr = document.querySelector(`.tr_${index}`);
-    let oldDate = document.querySelector(`#old-Date${index}`).value;
-    tr.children[1].innerHTML = oldDate;
-    let oldName = document.querySelector(`#old-Name${index}`).value;
-    tr.children[2].innerHTML = oldName;
-    let oldTime = document.querySelector(`#old-Time${index}`).value;
-    tr.children[3].innerHTML = oldTime;
-    let oldPhone = document.querySelector(`#old-Phone${index}`).value;
-    tr.children[4].innerHTML = oldPhone;
-    let oldPerson = document.querySelector(`#old-Person${index}`).value;
-    tr.children[5].innerHTML = oldPerson;
-    let oldEmail = document.querySelector(`#old-Email${index}`).value;
-    tr.children[6].innerHTML = oldEmail;
+function cancelBooking(index) {
+    // let tr = document.querySelector(`.tr_${index}`);
+    // let oldDate = document.querySelector(`#old-Date${index}`).value;
+    // tr.children[1].innerHTML = oldDate;
+    // let oldName = document.querySelector(`#old-Name${index}`).value;
+    // tr.children[2].innerHTML = oldName;
+    // let oldTime = document.querySelector(`#old-Time${index}`).value;
+    // tr.children[3].innerHTML = oldTime;
+    // let oldPhone = document.querySelector(`#old-Phone${index}`).value;
+    // tr.children[4].innerHTML = oldPhone;
+    // let oldPerson = document.querySelector(`#old-Person${index}`).value;
+    // tr.children[5].innerHTML = oldPerson;
+    // let oldEmail = document.querySelector(`#old-Email${index}`).value;
+    // tr.children[6].innerHTML = oldEmail;
+    renderOrderList();
     document.querySelector(`.save-booking${index}`).classList.add("d-none");
     document.querySelector(`.cancel-booking${index}`).classList.add("d-none");
     document.querySelector(`.edit-booking${index}`).classList.remove("d-none");
@@ -424,7 +493,7 @@ function checkLogin() {
     //     }
 
     // }
-    
+
     for (let login of loginList) {
         if (username == login.username && password == login.password) {
             loged = true;
@@ -465,7 +534,7 @@ function checkLogin() {
             item.classList.remove("d-none");
         });
 
-        // document.querySelector(".checkOrder").classList.remove('d-none');
+        document.querySelector(".checkOrder").classList.remove('d-none');
 
         document.querySelector(`#userName_2`).value = null;
         document.querySelector(`#passWord_2`).value = null;
@@ -474,10 +543,6 @@ function checkLogin() {
         document.querySelector('#userName_2').classList.add('d-none');
         document.querySelector('#passWord_1').classList.add('d-none');
         document.querySelector('#passWord_2').classList.add('d-none');
-
-        // document.querySelector("#removeMenuLunch").classList.remove('d-none');
-        // document.querySelector("#removeMenuNight").classList.remove('d-none');
-        // document.querySelector("#removeMenuDrink").classList.remove('d-none');
     }
     else {
         alert('Đăng Nhập Thất Bại Vui Lòng Thử Lại');
@@ -500,21 +565,16 @@ function pressEnter(i) {
 }
 // localStorage
 let key_order = "order_data";
-function init(){
-    if(getData(key_order) == null){
+function init() {
+    if (getData(key_order) == null) {
         orderList = []
         setData(key_order, orderList);
     } else {
         orderList = getData(key_order);
     }
 }
-function getData(key) {
-    return JSON.parse(localStorage.getItem(key))
-}
-function setData(key, data) {
-    localStorage.setItem(key, JSON.stringify(data))
-}
-function mainBooking(){
+
+function mainBooking() {
     init();
     renderOrderList();
 }
